866bbaf51d0134751da5f687ef68682c85939124
shobhit9070 <shobhitrock80@gmail.com>
2022-11-13 16:23:30 +0530

    nft smart contracts

---
+++ nftSmartContracts/contracts/Our_ERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DRExToken is ERC20 {
+    IERC20 private _token;
+    uint256 public totalTokenSupplied;
+
+    constructor(uint256 initialSupply) ERC20("DREx", "DRX") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function setAddress(IERC20 token) external {
+        _token = token;
+    }
+
+    function tranferingfunds(address payable to, uint256 amt) external {
+        totalTokenSupplied += amt;
+        _token.transfer(to, amt);
+    }
+}
